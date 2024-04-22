e54bc9176428fcd6b27ffc8bdcadef68e8040f54
youm <juliet0524@naver.com>
2024-04-22 14:27:45 +0900

    소수-에라토스테네스의 체 알고리즘

--- src/프로그래머스/level1/소수구하기.java
+++ src/프로그래머스/level1/소수구하기.java
@@ -7,23 +7,17 @@ public class 소수구하기 {
             int[] num = new int[n+1];
 
             //2~n까지 배열에 넣기
-            for(int i = 2; i<=n; i++){
-                num[i]=i;
-            }
+            for(int i = 2; i<=n; i++) num[i]=i;
 
-            for(int i = 2; i<= n ; i++){
+
+            for(int i = 2; i<= n ; i++) {
                 //0이면 패스
-                if(num[i] == 0) continue;
+                if (num[i] == 0) continue;
                 //2,3,5,7순으로 배수 다 0으로 만들기
-                for(int j = 2*i; j<= n; j+=i){
-                    num[j] = 0;
-                }
+                for (int j = 2 * i; j <= n; j += i) num[j] = 0;
                 //0 아니면 ++
-                if(num[i] != 0)
-                    answer++;
+                if (num[i] != 0) answer++;
             }
-
-
             return answer;
         }
     }
