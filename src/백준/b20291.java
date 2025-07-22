fe822ad65a77efbdb3bed298e7a6cd3a4111c5f9
youm <juliet0524@naver.com>
2025-07-23 00:12:06 +0900

    백준 20291_문자열

---
+++ src/백준/b20291.java
@@ -0,0 +1,30 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class b20291 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Map<String, Integer> count = new HashMap<>();
+
+        for(int i = 0; i<N; i++){
+            String s = br.readLine();
+            int index = s.indexOf(".");
+            String cut = s.substring(index+1);
+            count.put(cut,count.getOrDefault(cut,0)+1);
+        }
+        List<String> keySet = new ArrayList<>(count.keySet());
+
+        Collections.sort(keySet);
+
+        for(String key: keySet){
+            System.out.print(key+" ");
+            System.out.print(count.get(key));
+            System.out.println();
+        }
+    }
+}
