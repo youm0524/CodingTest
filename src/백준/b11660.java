8fbb9b38dcc9cc9e2d12a1954c078bc76de1ebb2
youm0524 <102306400+youm0524@users.noreply.github.com>
2025-08-12 13:38:35 +0900

    Update b11660.java

--- src/백준/b11660.java
+++ src/백준/b11660.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class b11660 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
