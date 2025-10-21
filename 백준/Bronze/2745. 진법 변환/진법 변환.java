fe9e1124a06517314364ca433d35e97d1de0305e
youm0524 <102306400+youm0524@users.noreply.github.com>
2025-10-21 09:00:11 +0900

    [Bronze II] Title: 진법 변환, Time: 108 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        String N = st.nextToken();
+        int B = Integer.parseInt(st.nextToken());
+        br.close();
+
+        int tmp = 1;
+        int sum = 0;
+        
+        for(int i = N.length()-1; i>=0; i--){
+            char c = N.charAt(i);
+            
+            if(c>='A' && c<='Z'){
+                sum += (c - 'A' + 10) * tmp;
+            }
+            else{
+                sum+= (c-'0') * tmp;
+            }
+            tmp*=B;
+        }
+         System.out.println(sum);
+
+        
+    }
+}
