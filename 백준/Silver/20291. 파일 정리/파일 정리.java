684cf529b8b689f2c4e19a5d7b339a9d4ae99183
youm0524 <102306400+youm0524@users.noreply.github.com>
2025-11-24 08:15:53 +0900

    [Silver III] Title: 파일 정리, Time: 1124 ms, Memory: 59932 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -7,22 +7,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        Map<String, Integer> count = new HashMap<>();
-
+        Map<String,Integer> maps = new TreeMap<>();
         for(int i = 0; i<N; i++){
             String s = br.readLine();
-            int index = s.indexOf(".");
-            String cut = s.substring(index+1);
-            count.put(cut,count.getOrDefault(cut,0)+1);
+            String[] ss = s.split("\\.");
+            String ex = ss[1];
+            maps.put(ex,maps.getOrDefault(ex,0)+1);
         }
-        List<String> keySet = new ArrayList<>(count.keySet());
-
-        Collections.sort(keySet);
-
-        for(String key: keySet){
-            System.out.print(key+" ");
-            System.out.print(count.get(key));
-            System.out.println();
+        
+        for(String key : maps.keySet()){
+            System.out.println(key+" "+maps.get(key));
+            
         }
     }
 }
