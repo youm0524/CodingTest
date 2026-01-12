7828f8a669d257dd24785c3681f919db7c6bd137
youm0524 <102306400+youm0524@users.noreply.github.com>
2026-01-12 14:12:28 +0900

    [Silver II] Title: 오리, Time: 124 ms, Memory: 14840 KB -BaekjoonHub

--- 백준/Silver/12933. 오리/오리.java
+++ 백준/Silver/12933. 오리/오리.java
@@ -1,44 +1,52 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    static char[] QUACK = {'q','u','a','c','k'};
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String sound = sc.next();
-
-        int[] visit = new int[sound.length()];
-        int answer = 0;
-        if(sound.length()%5!=0 || sound.charAt(0) != 'q') {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        
+        int len = s.length();
+        if (len % 5 != 0 || s.charAt(0) != 'q') {
             System.out.println(-1);
             return;
         }
-        for(int i = 0; i<sound.length(); i++){
-            if (visit[i]==1) continue;
-            int count = 0;
+        
+        String quack = "quack";
+        boolean[] visited = new boolean[len];
+        int ans = 0;
+        
+        for (int i = 0; i < len; ) {
+            if (visited[i]) {
+                i++;
+                continue;
+            }
+            
+            int cnt = 0;
             int idx = 0;
-            for(int j = i; j<sound.length(); j++){
-                if(visit[j]!=1 && QUACK[idx] == sound.charAt(j)){
-                    visit[j]=1;
-                    idx = (idx+1)%5;
-                    count++;
+            for (int j = i; j < len; j++) {
+                if (!visited[j] && s.charAt(j) == quack.charAt(idx)) {
+                    visited[j] = true;
+                    idx = (idx + 1) % 5;
+                    cnt++;
                 }
             }
-            if(count%5==0){
-                answer++;
-            }
-            else {
+            
+            if (cnt % 5 != 0) {
                 System.out.println(-1);
                 return;
             }
-
+            ans++;
         }
-        for (int i = 0; i < sound.length(); i++) {
-            if (visit[i] == 0) {
+        
+        for (boolean v : visited) {
+            if (!v) {
                 System.out.println(-1);
                 return;
             }
         }
-        System.out.println(answer);
-
+        
+        System.out.println(ans);
     }
 }
