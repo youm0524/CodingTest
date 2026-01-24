d338fa01470a245755e4b5bc86e43df8f662b201
youm0524 <102306400+youm0524@users.noreply.github.com>
2026-01-24 13:54:44 +0900

    Time: 20 ms (34.9%), Space: 98.2 MB (36.94%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -3,19 +3,19 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
         boolean[] visited = new boolean[n+1];
-        dfs(result, n, k, path,visited);
+        dfs(result, 1, n, k, path,visited);
         return result;
     }
-    public void dfs(List<List<Integer>> result, int n, int k, List<Integer> path, boolean[] visited){
+    public void dfs(List<List<Integer>> result, int start, int n, int k, List<Integer> path, boolean[] visited){
         if(path.size()==k){
             result.add(new ArrayList<>(path));
             return;
         }
-        for(int i = 1; i<n+1; i++){
+        for(int i = start; i<n+1; i++){
             if(!visited[i]){
             path.add(i);
             visited[i] = true;
-            dfs(result, i, k, path,visited);
+            dfs(result, i+1,n, k, path,visited);
             path.remove(path.size()-1);
             visited[i] = false;
             }            
