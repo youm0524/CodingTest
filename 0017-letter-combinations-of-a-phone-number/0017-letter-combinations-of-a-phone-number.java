5f33ea158ed05c8383a4428518b1ba53e4dda52f
youm0524 <102306400+youm0524@users.noreply.github.com>
2026-01-29 14:04:22 +0900

    Time: 0 ms (100%), Space: 43.3 MB (85.42%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,32 +1,25 @@
 class Solution {
-    public static HashMap<Character, String[]> nums;
+    public String[] letters = {"", "", "abc", "def", "ghi", "kjl", "mno", "pqrs", "tuv", "wxyz"};
+
     public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<>();
-        if(digits.length()==0)return result;
-        nums = new HashMap<>();
-        nums.put('2',new String[]{"a","b","c"});
-        nums.put('3',new String[]{"d","e","f"});
-        nums.put('4',new String[]{"g","h","i"});
-        nums.put('5',new String[]{"j","k","l"});
-        nums.put('6',new String[]{"m","n","o"});
-        nums.put('7',new String[]{"p","q","r","s"});
-        nums.put('8',new String[]{"t","u","v"});
-        nums.put('9',new String[]{"w","x","y","z"});
-        dfs(result,digits,0,new StringBuilder());
-        return result;
-
-
-
+        List<String> answer = new ArrayList<>();
+        dfs(answer, new StringBuilder(), digits.toCharArray(), 0);
+        return answer;
     }
-    public void dfs(List<String> result, String digits, int index, StringBuilder path){
-        if(path.length()==digits.length()){
-            result.add(String.valueOf(path));
+
+    public void dfs(List<String> answer, StringBuilder sb, char[] digits, int depth){
+        if(depth==digits.length){
+            answer.add(sb.toString());
             return;
         }
-
-        for(String c: nums.get(digits.charAt(index))){
-            dfs(result, digits,index+1,new StringBuilder(path).append(c));
+        char[] pick = letters[digits[depth]-'0'].toCharArray();
+        for(int i = 0; i<pick.length; i++){
+            char c = pick[i];
+            sb.append(c);
+            dfs(answer,sb,digits,depth+1);
+            sb.delete(sb.length() - 1, sb.length());
         }
     }
+    
 
 }
