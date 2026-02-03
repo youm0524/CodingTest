8b22e0aacfdd664cae1198e66238d312eb516084
youm0524 <102306400+youm0524@users.noreply.github.com>
2026-02-03 16:12:29 +0900

    [level 3] Title: 여행경로, Time: 0.59 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,28 +1,23 @@
 import java.util.*;
+
 class Solution {
-    private static boolean visited[];
-    private static ArrayList<String> list;
     public String[] solution(String[][] tickets) {
-        visited = new boolean[tickets.length];
-        int cnt = 0;
-        list = new ArrayList<String>();
-        dfs("ICN", "ICN", tickets, cnt);
         String[] answer = {};
-        Collections.sort(list);
-        answer = list.get(0).split(" ");
-        return answer;
-    }
-    public void dfs(String start, String route, String[][] tickets, int cnt){
-        if(cnt == tickets.length){
-            list.add(route);
-        }
-        for(int i = 0; i<tickets.length; i++){
-            if(!visited[i] && start.equals(tickets[i][0])){
-                visited[i] = true;
-                dfs(tickets[i][1], route + " " + tickets[i][1], tickets, cnt+1);
-                visited[i] = false;
-            }
+        List<String> path = new LinkedList<>();
+        Map<String, PriorityQueue<String>> map = new HashMap<>();
+        for(String[] ss : tickets){
+            map.putIfAbsent(ss[0],new PriorityQueue<>());
+            map.get(ss[0]).add(ss[1]);
         }
         
+        dfs("ICN", map, path);
+        answer = path.toArray(new String[0]);
+        return answer;
+    }
+    public void dfs(String from, Map<String, PriorityQueue<String>> map, List<String> path){
+        while(map.containsKey(from) && !map.get(from).isEmpty()){
+            dfs(map.get(from).poll(), map, path);
+        }
+        path.add(0,from);
     }
 }
