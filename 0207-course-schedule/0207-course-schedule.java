9a8b438c4d189841964a0c44ee5f7749af437a50
youm0524 <102306400+youm0524@users.noreply.github.com>
2026-02-04 16:25:28 +0900

    Time: 92 ms (5.23%), Space: 47 MB (75.57%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,37 +1,38 @@
 class Solution {
-    boolean[] visiting; // 현재 DFS 경로
-    boolean[] visited;  // 처리 완료
-
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        visiting = new boolean[numCourses];
-        visited = new boolean[numCourses];
+        Map<Integer, List<Integer>> finishToTakemap = new HashMap<>();
 
-        // 모든 노드에서 DFS 시작
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited[i]) {
-                if (!dfs(prerequisites, i)) return false;
+        for(int[] pre : prerequisites){
+            finishToTakemap.putIfAbsent(pre[0], new ArrayList<>());
+            finishToTakemap.get(pre[0]).add(pre[1]);
+        }
+
+        List<Integer> takes = new ArrayList<>();
+        //시간 초과 방지
+        List<Integer> taken = new ArrayList<>();
+        for(int key : finishToTakemap.keySet()){
+            if(!dfs(finishToTakemap, key,taken, takes)){
+                return false;
             }
         }
         return true;
+
     }
 
-    private boolean dfs(int[][] prerequisites, int course) {
-        // 현재 경로에 다시 들어오면 사이클
-        if (visiting[course]) return false;
-        if (visited[course]) return true;
-
-        visiting[course] = true;
-
-        for (int i = 0; i < prerequisites.length; i++) {
-            if (prerequisites[i][0] == course) {
-                if (!dfs(prerequisites, prerequisites[i][1])) {
-                    return false;
-                }
-            }
+    public boolean dfs(Map<Integer, List<Integer>> map,int key, List<Integer> taken, List<Integer> takes){
+        if(takes.contains(key)){
+            return false;
         }
+        if(taken.contains(key))return true;
 
-        visiting[course] = false;
-        visited[course] = true;
+        if(map.containsKey(key)){
+            takes.add(key);
+            for(int take : map.get(key)){
+                if(!dfs(map, take, taken, takes))return false;
+            }
+            takes.remove((Integer) key);
+            taken.add(key);
+        }
         return true;
     }
 }
