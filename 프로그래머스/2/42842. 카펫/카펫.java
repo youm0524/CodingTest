b4ceb28255974879c68474f7bb165fca378171ef
youm0524 <102306400+youm0524@users.noreply.github.com>
2026-02-09 16:22:02 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,23 +1,25 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int width = 0;
-        int height = 0;
-        for(int i = 1; i<=yellow; i++){
-            int x = 0;
-            int y = 0;
-            if(yellow%i==0){
-                x = i;
-                y = yellow/i;
-            }
-            if((2*(x+2) + 2*(y))==brown && x>=y){
-                width = x+2;
-                height = y+2;
-                break;
+        int total = brown + yellow;
+        for(int i = 1; i<total; i++){
+            int w = 0;
+            int h = 0;
+            if(total%i==0){
+                if(total/i>=i){
+                    w = total/i;
+                    h = i;
+                }else{
+                    w = i;
+                    h = total/i;
+                }
+                if((w-2)*(h-2) == yellow){
+                    answer[0] = w;
+                    answer[1] = h;
+                    break;
+                }
             }
         }
-        answer[0] = width;
-        answer[1] = height;
         return answer;
     }
 }
