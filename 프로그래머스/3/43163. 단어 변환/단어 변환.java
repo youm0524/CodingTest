175140e7af6e0c2b8df696c15ceb3102feb77928
youm0524 <102306400+youm0524@users.noreply.github.com>
2026-02-21 11:38:46 +0900

    [level 3] Title: 단어 변환, Time: 0.27 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,31 +1,30 @@
 class Solution {
-    static int answer =50;
+    boolean[] visited;
+    int answer = Integer.MAX_VALUE;
     public int solution(String begin, String target, String[] words) {
-        boolean[] visited = new boolean[words.length];
-        int count = 0;
-        dfs(begin, target, words,visited,count);
-        if(answer == 50)return 0;
+        visited = new boolean[words.length];
+        dfs(begin,target,words,0);
+        if(answer == Integer.MAX_VALUE){
+            answer = 0;
+        }
+        //System.out.println(answer);
         return answer;
     }
-    public void dfs(String begin, String target, String[] words, boolean[] visited, int count){
+    public void dfs(String begin, String target, String[] words, int cnt){
         if(begin.equals(target)){
-            if(count<answer)answer = count;
+            answer = Math.min(cnt,answer);
         }
-        for(int i = 0; i<words.length; i++){
-            int diff = 0;
-            for(int j = 0; j<words[i].length(); j++){
-                if(words[i].charAt(j)!=begin.charAt(j)){
-                    diff++;
-                    if(diff>2) break;
-                }
+        for(int j = 0; j<words.length; j++){
+            int count = 0;
+            String s = words[j];
+            for(int i = 0; i<s.length(); i++){
+                if(s.charAt(i)!=begin.charAt(i))count++;
             }
-            if(!visited[i] && diff==1){
-                visited[i] = true;
-                dfs(words[i], target,words,visited,count+1);
-                System.out.println(answer);
-                visited[i] = false;
+            if(count==1 && !visited[j]){
+                visited[j] = true;
+                dfs(s,target,words,cnt+1);
+                visited[j] = false;
             }
         }
-        
     }
 }
