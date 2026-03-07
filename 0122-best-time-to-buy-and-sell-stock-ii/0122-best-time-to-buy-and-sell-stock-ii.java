2cc575e1ec3cd011ee944ae9067d7b4650b02ab9
youm0524 <102306400+youm0524@users.noreply.github.com>
2026-03-07 14:13:48 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,27 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buy = 0;
-        int profit = 0;
-        boolean holding = false;
-
-        for (int i = 0; i < prices.length - 1; i++) {
-            if (!holding) {
-                if (prices[i] < prices[i + 1]) {
-                    buy = prices[i];
-                    holding = true;
-                }
-            } else {
-                if (prices[i] > prices[i + 1]) {
-                    profit += prices[i] - buy;
-                    holding = false;
-                }
+        int result = 0;
+        for(int i = 0; i<prices.length-1; i++){
+            if(prices[i+1]>prices[i]){
+                result+=prices[i+1]-prices[i];
             }
         }
-
-        if (holding) {
-            profit += prices[prices.length - 1] - buy;
-        }
-
-        return profit;
+        return result;
+        
     }
 }
