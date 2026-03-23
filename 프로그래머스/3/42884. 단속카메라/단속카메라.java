47ab7c3a411c4186bdd54d6d828903beaba2e2c4
youm0524 <102306400+youm0524@users.noreply.github.com>
2026-03-23 13:06:02 +0900

    [level 3] Title: 단속카메라, Time: 8.90 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,37 +2,16 @@ import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
         int answer = 1;
-        List<int[]> list = new ArrayList<>();
-        
-        Arrays.sort(routes,(a,b)->Integer.compare(a[1], b[1]));
-        list.add(routes[0]);
+        Arrays.sort(routes,(a,b)->{
+            return a[1]-b[1];
+        });
+        int start = routes[0][1];
         for(int i = 1; i<routes.length; i++){
-            int a = routes[i][0];
-            int b = routes[i][1];
-            int idx = 0;
-            int[] re = new int[2];
-            for(int[] li : list){
-                int start = li[0];
-                int end = li[1];
-                if(b<start || a>end){
-                    idx = 1;
-                    re[0] = a;
-                    re[1] = b;
-                    //System.out.println(start+" "+end);
-                }
-                else{
-                    idx = 0;
-                    break;
-                    //System.out.println(a+" "+b);
-                    //answer++;
-                }
-            }
-            if(idx==1){
+            if(routes[i][0]<=start){
+                continue;
+            }else{
+                start = routes[i][1];
                 answer++;
-                list.remove(re);
-                int k = a>re[0]?re[0]:a;
-                int p = b>re[1]?re[1]:b;
-                list.add(new int[]{k,p});
             }
         }
         return answer;
