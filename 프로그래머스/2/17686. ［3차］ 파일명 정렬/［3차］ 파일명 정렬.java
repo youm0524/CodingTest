a28dddcf35423cab42b52faeab5e04115660571a
youm0524 <102306400+youm0524@users.noreply.github.com>
2026-03-26 15:45:22 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 10.58 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,45 +1,50 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] files) {
-        Arrays.sort(files,(a,b)->{
-            int aIdx = 0;
-            int bIdx = 0;
-            // head 끝 찾기
-            while (aIdx < a.length() && !Character.isDigit(a.charAt(aIdx))){
-                aIdx++;
+        String[][] file = new String[files.length][3];
+        
+        for(int i = 0; i<files.length; i++){
+            String s = files[i];
+            int idx = 0;
+            while(idx<s.length() && !Character.isDigit(s.charAt(idx))){
+                idx++;
             }
-            while(bIdx<b.length() && !Character.isDigit(b.charAt(bIdx))){
-                bIdx++;
+            //System.out.println(idx);
+            String head = (s.substring(0,idx)).toLowerCase();
+            file[i][0] = head;
+            int start = idx;
+            int count = 0;
+            while(idx<s.length()&& count<5 && Character.isDigit(s.charAt(idx))){
+                idx++;
+                count++;
             }
-            String aHead = a.substring(0,aIdx).toLowerCase();
-            String bHead = b.substring(0,bIdx).toLowerCase();
-            int headCompare = aHead.compareTo(bHead);
-            if (headCompare != 0) {
+            String tail = s.substring(start,idx);
+            file[i][1] = tail;
+            file[i][2] = s;
+        }
+        Arrays.sort(file,(a,b)->{
+            int headCompare = a[0].compareTo(b[0]);
+            if(headCompare!=0){
                 return headCompare;
             }
-            
-            int aNumEnd = aIdx;
-            int bNumEnd = bIdx;
-            int aCount = 0;
-            int bCount = 0;
-            
-            while (aNumEnd < a.length() && Character.isDigit(a.charAt(aNumEnd)) && aCount < 5) {
-                aNumEnd++;
-                aCount++;
-            }
-            while (bNumEnd < b.length() && Character.isDigit(b.charAt(bNumEnd)) && bCount < 5) {
-                bNumEnd++;
-                bCount++;
-            }
-            
-            int aNumber = Integer.parseInt(a.substring(aIdx, aNumEnd));
-            int bNumber = Integer.parseInt(b.substring(bIdx, bNumEnd));
-            
-            // 2. number 비교
-            return aNumber - bNumber;
+            int tailA = Integer.parseInt(a[1]);
+            int tailB = Integer.parseInt(b[1]);
+            return tailA-tailB;
         });
+        String[] answer = new String[files.length];
+        for (int i = 0; i < files.length; i++) {
+            answer[i] = file[i][2];
+        }
         
-        return files;
+        return answer;
+
+//         for(int i = 0; i<file.length; i++){
+            
+//             String head = file[i][0];
+//             String tail = file[i][1];
+            
+//             System.out.println(head+" "+tail);
+//         }
         
     }
 }
