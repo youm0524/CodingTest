9641e5e37e935972fc53726e537d461734c8f8f9
youm0524 <102306400+youm0524@users.noreply.github.com>
2026-04-10 16:39:38 +0900

    [level 3] Title: 섬 연결하기, Time: 0.92 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
 
 ### 성능 요약
 
-메모리: 75.3 MB, 시간: 0.64 ms
+메모리: 76.8 MB, 시간: 0.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 06일 20:24:40
+2026년 04월 10일 16:39:35
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,30 +2,33 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        Arrays.sort(costs, (a,b)-> a[2]-b[2]);
-        int[] parent = new int[n+1];
-        for(int i = 0; i<n; i++){
+        //크루스칼
+        Arrays.sort(costs,(a,b)->a[2]-b[2]);
+        int[] parent = new int[n];
+        
+        for(int i=0; i<n; i++){
             parent[i] = i;
-        }
-        int cnt = 0;
+        }        
         for(int[] cost : costs){
             int a = cost[0];
             int b = cost[1];
             int c = cost[2];
-            
-            int pa = find(parent, a);
-            int pb = find(parent, b);
-
-            if (pa != pb) {
-                parent[pb] = pa;
+            if(find(parent, a)!=find(parent, b)){
+                union(parent, a,b);
                 answer+=c;
             }
-            
         }
+        
         return answer;
     }
-    public int find(int[] parent, int x) {
-        if(parent[x] == x)return x;
-        else return find(parent,parent[x]);
+    public int find(int[] parent, int a){
+        if(parent[a]==a)return a;
+        else return find(parent, parent[a]);
+    }
+    
+    public void union(int[] parent, int a, int b){
+        int pa = find(parent, a);
+        int pb = find(parent, b);
+        parent[pb] = pa;
     }
 }
