a3854b625b199a322a13a8a314feb4013ed1d158
youm0524 <102306400+youm0524@users.noreply.github.com>
2026-04-21 12:26:21 +0900

    [level 2] Title: 스킬트리, Time: 0.87 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,46 +1,36 @@
 import java.util.*;
-
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-
-        for (String tree : skill_trees) {
-            Queue<String> queue = new LinkedList<>();
-
-            for (int i = 0; i < tree.length(); i++) {
-                String c = String.valueOf(tree.charAt(i));
-                if (skill.contains(c)) {
-                    queue.add(c);
-                }
-            }
-
-            String out = "";
-            boolean valid = true;
-
-            while (!queue.isEmpty()) {
-                String ss = queue.poll();
-                out += ss;
-
-                int idx = 0;
-                for (int i = 0; i < skill.length(); i++) {
-                    if (ss.equals(String.valueOf(skill.charAt(i)))) {
-                        idx = i;
-                        break;
+        
+        for(String s : skill_trees){
+            List<Character> list = new ArrayList<>();
+            boolean valid = false;
+            for(int i=0; i<s.length(); i++){
+                if(skill.contains(String.valueOf(s.charAt(i)))){
+                    int idx = 0;
+                    for(int j = 0; j<skill.length(); j++){
+                        if(skill.charAt(j)==s.charAt(i)){
+                            idx = j;
+                        }
                     }
-                }
-
-                if (idx != 0) {
-                    String need = skill.substring(0, idx);
-                    if (!out.contains(need)) {
-                        valid = false;
-                        break;
+                    if(idx!=0){
+                        char c = skill.charAt(idx-1);
+                        System.out.println(c);
+                        if(!list.contains(c)){
+                            //System.out.println("out");
+                            valid = true;
+                            break;
+                        }
                     }
+                    list.add(s.charAt(i));
+                    //System.out.println("aa");
                 }
+                
             }
-
-            if (valid) answer++;
+            if(!valid)answer++;
+            //System.out.println(answer);
         }
-
         return answer;
     }
 }
