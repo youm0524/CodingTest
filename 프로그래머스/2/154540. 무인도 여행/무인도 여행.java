0e3ea2ce5903e681bedd3f2c84fde33a31a2450f
youm0524 <102306400+youm0524@users.noreply.github.com>
2026-05-06 12:47:56 +0900

    [level 2] Title: 무인도 여행, Time: 9.36 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,60 +1,53 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String[] maps) {
+        int[] dx = {-1,0,1,0};
+        int[] dy = {0,1,0,-1};
         int n = maps.length;
         int m = maps[0].length();
-        
-        List<Integer> list = new ArrayList<>();
+        int x = 0;
+        int y = 0;
+        Character[][] map = new Character[n][m];
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                map[i][j] = maps[i].charAt(j);
+            }
+        }
         boolean[][] visited = new boolean[n][m];
-        
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, 1, 0, -1};
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                
-                if (!visited[i][j] && maps[i].charAt(j) != 'X') {
-                    
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0;i <n; i++){
+            for(int j = 0; j<m; j++){
+                if(map[i][j]!='X' && !visited[i][j]){
                     Queue<int[]> queue = new LinkedList<>();
-                    queue.add(new int[]{i, j});
+                    queue.add(new int[]{i,j});
                     visited[i][j] = true;
-                    
-                    int sum = maps[i].charAt(j) - '0';
-                    
-                    while (!queue.isEmpty()) {
-                        int[] cur = queue.poll();
-                        int x = cur[0];
-                        int y = cur[1];
-                        
-                        for (int k = 0; k < 4; k++) {
-                            int nx = x + dx[k];
-                            int ny = y + dy[k];
-                            
-                            if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                                if (!visited[nx][ny] && maps[nx].charAt(ny) != 'X') {
-                                    visited[nx][ny] = true;
-                                    queue.add(new int[]{nx, ny});
-                                    sum += maps[nx].charAt(ny) - '0';
-                                }
+                    int sum = map[i][j] - '0';
+                    while(!queue.isEmpty()){
+                        int[] li = queue.poll();
+                        int xx = li[0];
+                        int yy = li[1];
+                        for(int k = 0; k<4; k++){
+                            int nx = xx +dx[k];
+                            int ny = yy +dy[k];
+                            if(nx>=0 && nx<n && ny>=0 && ny<m &&
+                               !visited[nx][ny] && map[nx][ny]!='X'){
+                                visited[nx][ny] = true;
+                                sum+= map[nx][ny] - '0';
+                                queue.add(new int[]{nx,ny});
                             }
                         }
                     }
-                    
                     list.add(sum);
-                }
+                } 
             }
         }
-        
-        if (list.isEmpty()) return new int[]{-1};
-        
-        Collections.sort(list);
-        
         int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
+        Collections.sort(list);
+        if(list.isEmpty()) return new int[]{-1};
         
+        for(int i = 0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }  
         return answer;
     }
 }
