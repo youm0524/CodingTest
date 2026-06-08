ec1ece91e6db43b3567b599bdf62512b6bb2ddf0
youm0524 <102306400+youm0524@users.noreply.github.com>
2026-06-08 17:15:13 +0900

    [level 2] Title: 광물 캐기, Time: 0.91 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -3,50 +3,42 @@ class Solution {
     public int solution(int[] picks, String[] minerals) {
         int answer = 0;
         int totalPicks = picks[0] + picks[1] + picks[2];
-        
-        int max = Math.min(minerals.length, totalPicks*5);
-        int groupcnt = (max+4)/5;
-        int[][] cnt = new int[groupcnt][4];
-        for(int i = 0; i<groupcnt; i++){
-            for(int j = 0; j<5; j++){
-                int idx = i*5 + j;
-                if(idx >= max) break;
-                String s = minerals[idx];
-                if(s.equals("diamond")){
-                    cnt[i][0]++;
-                    cnt[i][3]+=25;
-                }else if(s.equals("iron")){
-                    cnt[i][1]++;
-                    cnt[i][3]+=5;
-                }else{
-                    cnt[i][2]++;
-                    cnt[i][3]+=1;
-                }
-            } 
-        }
-        Arrays.sort(cnt,(a,b)->b[3]-a[3]);
-        
-        int idx = 0;
-
-        while (idx < groupcnt && picks[0] > 0) {
-            answer += cnt[idx][0] + cnt[idx][1] + cnt[idx][2];
-            picks[0]--;
-            idx++;
-        }
-
-        while (idx < groupcnt && picks[1] > 0) {
-            answer += cnt[idx][0] * 5 + cnt[idx][1] + cnt[idx][2];
-            picks[1]--;
-            idx++;
-        }
-
-        while (idx < groupcnt && picks[2] > 0) {
-            answer += cnt[idx][0] * 25 + cnt[idx][1] * 5 + cnt[idx][2];
-            picks[2]--;
-            idx++;
+        int maxMinerals = Math.min(minerals.length, totalPicks * 5);
+        int groupCnt = (maxMinerals + 4) / 5;
+        int[][] mineral = new int[groupCnt][4];
+        for(int i = 0; i<maxMinerals; i++){
+            int idx = i / 5;
+            if(minerals[i].equals("diamond")){
+                mineral[idx][0]++;
+                mineral[idx][3]+=25;
+            }else if(minerals[i].equals("iron")){
+                mineral[idx][1]++;
+                mineral[idx][3]+=5;
+            }else{
+                mineral[idx][2]++;
+                mineral[idx][3]+=1;
+            }
         }
         
+        Arrays.sort(mineral,(a,b)->b[3]-a[3]);
         
+        for(int i = 0; i<groupCnt; i++){
+            if(picks[0]>0){
+                answer+=mineral[i][0] + mineral[i][1] + mineral[i][2];
+                picks[0]--;
+                System.out.println(answer);
+            }
+            else if(picks[1]>0){
+                answer+= mineral[i][0]*5 + mineral[i][1] + mineral[i][2];
+                picks[1]--;
+                System.out.println(answer);
+            }
+            else if(picks[2]>0){
+                answer+= mineral[i][0]*25 + mineral[i][1]*5 + mineral[i][2];
+                picks[2]--;
+            }
+            
+        }
         return answer;
     }
 }
