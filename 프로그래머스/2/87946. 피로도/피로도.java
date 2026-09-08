4b9b2c522743e5e68a0e12d897441d92d05afed7
youm0524 <102306400+youm0524@users.noreply.github.com>
2026-09-08 20:22:45 +0900

    [level 2] Title: 피로도, Time: 8.82 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,25 +1,51 @@
+import java.util.*;
 class Solution {
-    static boolean visited[];
-    static int cnt;
-    static int answer= 0;
+    static int dunLen;
+    static int max;
+    static int K;
+    static int[][] dungeon;
+    static boolean[] visited;
     public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
+        int answer = -1;
+        K = k;
+        dungeon = dungeons;
+        dunLen = dungeons.length;
+        visited = new boolean[dunLen];
+        max = Integer.MIN_VALUE;
+        List<Integer> list = new ArrayList<>();
+        dfs(list);
+        //System.out.println(max);
+        return max;
+    }
+    public void dfs(List<Integer> list){
+        if(list.size()==dunLen){
+            max = Math.max(max,check(dungeon, list));
+            //System.out.println(max);
+            return;
+        }
+        for(int i = 0; i<dunLen; i++){
+            if(visited[i])continue;
+            list.add(i);
+            visited[i]=true;
+            dfs(list);
+            list.remove(list.size()-1);
+            visited[i] = false;
+        }
+    }
+    public int check(int[][] dungeons, List<Integer> list){
+        int remain = K;
         int cnt = 0;
         
-        dfs(k, dungeons, cnt);
-        return answer;
-    }
-    public void dfs(int k, int[][] dungeons, int cnt){
-        answer = Math.max(answer, cnt);
-        for(int i = 0; i<dungeons.length;i++){
-            if(k>=dungeons[i][0] && !visited[i]){
-                visited[i] = true;
-                int tmp = k-dungeons[i][1];
+        for(int i : list){
+            if(remain>= dungeons[i][0]){
+                remain-=dungeons[i][1];
+                cnt++;
                 //System.out.println(cnt);
-                dfs(tmp, dungeons, cnt+1);
-                visited[i] = false;
+            }else{
+                return cnt;
             }
         }
-        return;
+        //System.out.println(cnt);
+        return cnt;
     }
 }
