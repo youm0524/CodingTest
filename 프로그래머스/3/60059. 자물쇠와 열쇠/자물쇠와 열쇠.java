642fe97b8d750e933c71b43a63023f7c71f6ccad
youm0524 <102306400+youm0524@users.noreply.github.com>
2026-09-22 14:48:43 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 17.25 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,55 +1,45 @@
-import java.util.*;
 class Solution {
     public boolean solution(int[][] key, int[][] lock) {
-        int m = key.length;
-        int n = lock.length;
-        int boardsize = 3*n;
-        int[][] board = new int[boardsize][boardsize];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                board[n + i][n + j] = lock[i][j];
+        boolean answer = false;
+        int N = lock.length;
+        int M = key.length;
+        int size = 2*M + N;
+        int[][] board = new int[size][size];
+        for(int i = M; i<M+N; i++){
+            for(int j = M; j<M+N; j++){
+                board[i][j] = lock[i-M][j-M];
             }
         }
-        
-        // 0도, 90도, 180도, 270도
-        for (int r = 0; r < 4; r++) {
-
-            // key를 올릴 수 있는 모든 위치 탐색
-            for (int i = 0; i <= boardsize - m; i++) {
-                for (int j = 0; j <= boardsize - m; j++) {
-
-                    // key 올리기
-                    for (int x = 0; x < m; x++) {
-                        for (int y = 0; y < m; y++) {
-                            board[i + x][j + y] += key[x][y];
+        for(int r = 0; r<4; r++){
+            
+            for(int i = 0; i<size-M; i++){
+                for(int j = 0; j<size-M; j++){
+                    for(int k = 0; k<M; k++){
+                        for(int l = 0; l<M; l++){
+                            board[i+k][j+l] += key[k][l];
                         }
                     }
-
-                    // 자물쇠 영역 검사
-                    if (check(board, n)) {
+                    if(check(board, M, N)){
                         return true;
                     }
-
-                    // 원상복구
-                    for (int x = 0; x < m; x++) {
-                        for (int y = 0; y < m; y++) {
-                            board[i + x][j + y] -= key[x][y];
+                    //원상 복귀
+                    for(int k = 0; k<M; k++){
+                        for(int l = 0; l<M; l++){
+                            board[i+k][j+l] -= key[k][l];
                         }
                     }
                 }
             }
-
-            // 90도 회전
             key = rotate(key);
+            
         }
         
-        return false;
+        return answer;
     }
-    
-    public boolean check(int[][] board, int n){
-        for(int i = n; i<n*2; i++){
-            for(int j = n; j<n*2; j++){
-                if(board[i][j]!=1){
+    public boolean check(int[][] board, int M, int N){
+        for(int i = M; i<M+N; i++){
+            for(int j = M; j<M+N; j++){
+                if(board[i][j] != 1){
                     return false;
                 }
             }
@@ -57,12 +47,13 @@ class Solution {
         return true;
     }
     public int[][] rotate(int[][] key){
-        int m = key.length;
-        int[][] result = new int[m][m];
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < m; j++) {
-                result[j][m - 1 - i] = key[i][j];
+        int M = key.length;
+        int[][] result = new int[M][M];
+
+        for(int i = 0; i < M; i++){
+            for(int j = 0; j < M; j++){
+                result[j][M - 1 - i] = key[i][j];
             }
         }
 
